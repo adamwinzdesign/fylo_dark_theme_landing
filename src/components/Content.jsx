0c44b6dc56fab30fba964af5928091e654870ff8
adamwinzdesign <adamwinzdesign@gmail.com>
2022-06-15 19:54:35 -0500

    minor error correction for deployment

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,7 @@
 import Hero from "./Hero";
 import Features from "./Features";
 import Team from "./Team";
-import SignIn from "./SignIn";
+// import SignIn from "./SignIn";
 import Footer from "./Footer";
 import Container from "./shared/Container";
 
--- src/components/shared/Img.jsx
+++ src/components/shared/Img.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { white } from "../../colors";
 
 const Img = styled.img`
 	&.hero {
